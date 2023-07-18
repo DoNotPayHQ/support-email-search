341cc702b7375dd714c277346e8bfccbe9669cf8
terryli0095 <terryli0095@gmail.com>
2023-07-17 18:07:21 -0700

    add second condition

--- src/api/routes/search.ts
+++ src/api/routes/search.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getEmails, getEmail} from 'business/search';
+import {getEmails, getEmail, getEmails2, combineEmails} from 'business/search';
 import {asyncHandler} from 'api/utils';
 import {BadRequestError} from 'api/exception';
 import {ERROR_CODE} from 'utils/constants';
@@ -13,7 +13,19 @@ router.get('/', asyncHandler(async function (req, res) {
 	const startDate = req.query.startDate || null;
 	const endDate = req.query.endDate || null;
 	const latest = req.query.latest || false;
-	const results = await getEmails(searchString, ticketBy, startDate, endDate, latest);
+	const secondSearchString = req.query.secondSearchString || null;
+	const secondTicketBy = req.query.secondTicketBy || null;
+	const secondLatest = req.query.secondLatest || null;
+	console.log('first', searchString, ticketBy, startDate)
+	console.log('second', secondSearchString, secondTicketBy, secondLatest)
+	let results = {}
+	if (!secondSearchString) {
+		results = await getEmails(searchString, ticketBy, startDate, endDate, latest );
+	} else {
+		const emailList1 = await getEmails(searchString, ticketBy, startDate, endDate, latest);
+		const emailList2 = await getEmails2(secondSearchString, secondTicketBy, startDate, endDate, secondLatest );
+		results = combineEmails(emailList1.data, emailList2.data);
+	}
 	res.send(results);
 }));
 
--- src/business/search/index.ts
+++ src/business/search/index.ts
@@ -5,7 +5,7 @@ interface EmailThread {
 }
 const readStream = fs.createReadStream('./emails.txt', {encoding: 'utf8', highWaterMark: 1024 * 1024 * 30});
 const emailThreads = [];
-let errors = 0
+let errors = 0;
 readStream.on('data', (chunk) => {
 	// process the data chunk
 	let buffer = '';
@@ -16,7 +16,7 @@ readStream.on('data', (chunk) => {
 		try {
 			parsedLine = JSON.parse(line);
 		} catch (error) {
-			errors=errors+1
+			errors = errors + 1;
 		}
 		return parsedLine;
 	});
@@ -28,7 +28,7 @@ readStream.on('data', (chunk) => {
 });
 
 readStream.on('end', () => {
-	console.log(errors)
+	console.log(errors);
 	console.log('file has been read completely');
 });
 
@@ -46,11 +46,11 @@ function addFilter(channel, filter) {
 			return true;
 		}
 	}
-	return false
+	return false;
 }
 export async function getEmail(id) {
-	const idInt =Number(id)
-	console.log('length', emailThreads.length)
+	const idInt = Number(id);
+	console.log('length', emailThreads.length);
 	const emailThread = emailThreads.find((emailThread) => {
 		return emailThread.id === idInt;
 	});
@@ -70,16 +70,47 @@ export async function getEmail(id) {
 		comments: emailThread.comments
 	};
 }
+export const combineEmails = (emailList1, emailList2) => {
+	// find the common emails by id
+	const email2Ids = emailList2.map((email2) => {
+		return email2.id;
+	});
+	const email1Ids = emailList1.map((email1) => {
+		return email1.id;
+	});
+	const commonEmails = emailList1.filter((email1) => {
+		return email2Ids.includes(email1.id);
+	});
+	const counters = {count: 0, byDates: {}};
+	for (const email of commonEmails) {
+		counters.count++;
+		const createdAt = new Date(email.createdAt);
+		if (counters['byDates'][`${createdAt.getUTCMonth() + 1}-${createdAt.getFullYear()}`]) {
+			counters['byDates'][`${createdAt.getUTCMonth() + 1}-${createdAt.getFullYear()}`]++;
+		} else {
+			counters['byDates'][`${createdAt.getUTCMonth() + 1}-${createdAt.getFullYear()}`] = 1;
+		}
+	}
+	return {
+		counters,
+		data: commonEmails
+	}
+};
 
-export async function getEmails(searchString: string, ticketBy?: string, fromDate?: string, toDate?: string, latest?: boolean) {
-	const filter ={}
+export async function getEmails2(
+	searchString: string,
+	ticketBy?: string,
+	fromDate?: string,
+	toDate?: string,
+	latest?: boolean
+) {
+	const filter = {};
 	if (ticketBy === 'user') {
-		filter['channel'] = 'email'
+		filter['channel'] = 'email';
 	} else if (ticketBy === 'support') {
-		filter['channel'] = 'web'
+		filter['channel'] = 'web';
 	}
 	console.log('searchString', searchString);
-	console.log('filter', filter);
 	let andOr = 'or';
 	let searchStringItems;
 	if (searchString.includes('&&')) {
@@ -113,9 +144,9 @@ export async function getEmails(searchString: string, ticketBy?: string, fromDat
 		if (emailThread.comments?.length === 0) {
 			continue;
 		}
-		let comments = emailThread.comments
+		let comments = emailThread.comments;
 		if (latest) {
-			comments = [comments[comments.length - 1]]
+			comments = [comments[comments.length - 1]];
 		}
 		let fxxaa = [];
 		if (andOr === 'and') {
@@ -155,7 +186,108 @@ export async function getEmails(searchString: string, ticketBy?: string, fromDat
 				requester: emailThread.requester?.name,
 				id: emailThread.id,
 				group: emailThread.group?.name,
-				description: emailThread.description,
+				description: emailThread.description
+			};
+			results.push(singleOutput);
+		}
+	}
+	return {
+		counters,
+		data: results
+	};
+}
+
+export async function getEmails(
+	searchString: string,
+	ticketBy?: string,
+	fromDate?: string,
+	toDate?: string,
+	latest?: boolean,
+) {
+	const filter = {};
+	if (ticketBy === 'user') {
+		filter['channel'] = 'email';
+	} else if (ticketBy === 'support') {
+		filter['channel'] = 'web';
+	}
+	console.log('searchString', searchString);
+	console.log('filter', filter);
+	let andOr = 'or';
+	let searchStringItems;
+	if (searchString.includes('&&')) {
+		searchStringItems = searchString.split('&&');
+		andOr = 'and';
+	} else if (searchString.includes('||')) {
+		searchStringItems = searchString.split('||');
+	} else {
+		searchStringItems = [searchString];
+	}
+	// trip white space
+	searchStringItems = searchStringItems.map((searchStringItem) => {
+		return searchStringItem.trim();
+	});
+	const counters = {count: 0, byDates: {}};
+	const results = [];
+	for (const emailThread of emailThreads) {
+		const createdAt = new Date(emailThread.created_at);
+		if (fromDate) {
+			const fromDateObj = new Date(fromDate);
+			if (createdAt < fromDateObj) {
+				continue;
+			}
+		}
+		if (toDate) {
+			const toDateObj = new Date(toDate);
+			if (createdAt > toDateObj) {
+				continue;
+			}
+		}
+		if (emailThread.comments?.length === 0) {
+			continue;
+		}
+		let comments = emailThread.comments;
+		if (latest) {
+			comments = [comments[comments.length - 1]];
+		}
+		let fxxaa = [];
+		if (andOr === 'and') {
+			fxxaa = comments.filter((c: any) => {
+				if (
+					addFilter(c.via.channel, filter) &&
+					searchStringItems.map((i) => c.plain_body.toLowerCase().includes(i)).every((v) => v === true)
+				) {
+					return true;
+				}
+				return false;
+			});
+		} else {
+			fxxaa = comments.filter((c: any) => {
+				if (
+					addFilter(c.via.channel, filter) &&
+					searchStringItems.map((i) => c.plain_body.toLowerCase().includes(i)).includes(true)
+				) {
+					return true;
+				}
+				return false;
+			});
+		}
+		if (fxxaa.length > 0) {
+			counters.count++;
+			if (counters['byDates'][`${createdAt.getUTCMonth() + 1}-${createdAt.getFullYear()}`]) {
+				counters['byDates'][`${createdAt.getUTCMonth() + 1}-${createdAt.getFullYear()}`]++;
+			} else {
+				counters['byDates'][`${createdAt.getUTCMonth() + 1}-${createdAt.getFullYear()}`] = 1;
+			}
+		}
+		if (fxxaa.length > 0) {
+			const singleOutput = {
+				createdAt: emailThread.created_at,
+				subject: emailThread.subject,
+				updatedAt: emailThread.updated_at,
+				requester: emailThread.requester?.name,
+				id: emailThread.id,
+				group: emailThread.group?.name,
+				description: emailThread.description
 			};
 			results.push(singleOutput);
 		}
